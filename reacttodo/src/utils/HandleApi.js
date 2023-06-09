167e5512f36bd81de06fc2e4cb2881c50f585aaa
AsmaaElshahat <39085840+AsmaaElshahat@users.noreply.github.com>
2023-06-09 12:52:16 +0100

    Create React App

---
+++ reacttodo/src/utils/HandleApi.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost:8000/"
+
+const getAllToDo = (setToDo) => {
+    axios
+        .get(baseUrl)
+        .then(({ data }) => {
+            console.log('data ---> ', data);
+            setToDo(data)
+        })
+}
+
+const addToDo = (text, setText, setToDo) => {
+
+    axios
+        .post(`${baseUrl}add/`, { "name": text })
+        .then((data) => {
+            console.log(data);
+            setText("")
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+
+    axios
+        .post(`${baseUrl}update/${toDoId}`, { todo_id: toDoId, "name":text })
+        .then((data) => {
+            setText("")
+            setIsUpdating(false)
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+const deleteToDo = (_id, setToDo) => {
+
+    axios
+        .delete(`${baseUrl}delete/${_id}`, { todo_id: _id })
+        .then((data) => {
+            console.log(data)
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+
+export { getAllToDo, addToDo, updateToDo, deleteToDo }
